76459efd8434924405d11744c1fde87b5948560c
Eric Florenzano <floguy@gmail.com>
2010-01-30 18:00:48 -0800

    Various refactorings

--- sfpy/superfeedr.py
+++ sfpy/superfeedr.py
@@ -4,50 +4,49 @@ from xmpp.protocol import Iq
 import xml.etree.ElementTree as ET
 
 class SuperFeedr(object):
-    def __init__(self,jid,password,debug=False, hostname='superfeedr.com'):
+    def __init__(self, jid, password, debug=False, hostname='superfeedr.com'):
         if debug:
-            self.client = xmpp.Client(server=self.hostname)
+            kwargs = {}
         else:
-            self.client = xmpp.Client(server=self.hostname,debug=[])
-        self.client.connect(server=(self.hostname,5222))
+            kwargs = {'debug': []}
+        self.client = xmpp.Client(server=self.hostname, **kwargs)
+        self.client.connect(server=(self.hostname, 5222))
         self.jid = jid
-        name = xmpp.protocol.JID(jid)
-        self.client.auth(name.getNode(), password)
+        self.client.auth(xmpp.protocol.JID(jid).getNode(), password)
         self.client.sendInitPresence()
         
         self.client.RegisterHandler('message',self.superfeedr_msg)
-        self.func = str
+        self.callback = lambda x: x
         
         self.hostname = hostname
-        return
 
-    def subscribe(self,feed):
-        data = Iq(typ='set',to=self.hostname,frm=self.jid)
-        child = data.addChild('pubsub',namespace='http://jabber.org/protocol/pubsub')
+    def subscribe(self, feed, hostname=None, sleep_time=1):
+        if not hostname:
+            hostname = 'firehoser.superfeedr.com'
+        data = Iq(typ='set', to=hostname, frm=self.jid)
+        child = data.addChild('pubsub',
+            namespace='http://jabber.org/protocol/pubsub')
         child.addChild('subscribe', {'node': feed, 'jid': self.jid})
         self.client.send(data)
-        time.sleep(1)
+        if sleep_time:
+            time.sleep(sleep_time)
         self.client.Process(1)
-        return 1
 
-    def unsubscribe(self,feed):
-        data = Iq(typ='set',to=self.hostname,frm=self.jid)
-        child = data.addChild('pubsub',namespace='http://jabber.org/protocol/pubsub')
+    def unsubscribe(self, feed, hostname=None, sleep_time=1):
+        if not hostname:
+            hostname = 'firehoser.superfeedr.com'
+        data = Iq(typ='set', to=hostname, frm=self.jid)
+        child = data.addChild('pubsub',
+            namespace='http://jabber.org/protocol/pubsub')
         child.addChild('unsubscribe', {'node': feed, 'jid': self.jid})
         self.client.send(data)
-        time.sleep(1)
+        if sleep_time:
+            time.sleep(sleep_time)
         self.client.Process(1)
-        return 1
     
     def monitor(self):
         while True:
-            try:
-                r= self.client.Process(1)
-            except Exception as e:
-		#log.debug( 'exception occurred:'+ str(e))
-                #log.debug('continuing...')
-                pass
-        return
+            self.client.Process(1)
 
     def superfeedr_msg(self,con,m):
         mstr = m.__str__()
@@ -59,34 +58,45 @@ class SuperFeedr(object):
         next_fetchx = xml.find('{http://jabber.org/protocol/pubsub#event}event/{http://superfeedr.com/xmpp-pubsub-ext}status/{http://superfeedr.com/xmpp-pubsub-ext}next_fetch')
         itemsx = xml.find('{http://jabber.org/protocol/pubsub#event}event/{http://jabber.org/protocol/pubsub#event}items')
         entriesx = xml.findall('{http://jabber.org/protocol/pubsub#event}event/{http://jabber.org/protocol/pubsub#event}items/{http://jabber.org/protocol/pubsub#event}item/{http://www.w3.org/2005/Atom}entry')
-        #print dict(statusx=statusx, httpx=httpx, next_fetchx=next_fetchx, itemsx=itemsx,entriesx= entriesx)
         if None not in (statusx, httpx, next_fetchx, itemsx, entriesx):
-            event['xml'] = xml
-            event['feed'] = itemsx.get('node')
-            event['http'] = (httpx.get('code'), httpx.text)
-            event['next_fetch'] = next_fetchx.text
-            event['entries'] = []
+            event.update({
+                'xml': xml,
+                'feed': itemsx.get('node'),
+                'http': (httpx.get('code'), httpx.text),
+                'next_fetch': next_fetchx.text,
+                'entries': [],
+            })
             for entryx in entriesx:
-                entry = {'title': '', 'summary':'','link':('','',''), 'id':'','published':''}
+                entry = {
+                    'title': '',
+                    'summary': '',
+                    'link': ('', '', ''),
+                    'id': '',
+                    'published': '',
+                }
                 titlex = entryx.find('{http://www.w3.org/2005/Atom}title')
                 summaryx = entryx.find('{http://www.w3.org/2005/Atom}summary')
                 linkx = entryx.find('{http://www.w3.org/2005/Atom}link')
                 idx = entryx.find('{http://www.w3.org/2005/Atom}id')
                 publishedx = entryx.find('{http://www.w3.org/2005/Atom}published')
-                if titlex is not None:
+                if titlex:
                     entry['title'] = titlex.text
-                    if summaryx is not None:
+                    if summaryx:
                         entry['summary'] = summaryx.text
-                    if linkx is not None:
-                        entry['link'] = (linkx.get('rel'), linkx.get('type'), linkx.get('href'))
-                    if idx is not None:
+                    if linkx:
+                        entry['link'] = (
+                            linkx.get('rel'),
+                            linkx.get('type'),
+                            linkx.get('href'),
+                        )
+                    if idx:
                         entry['id'] = idx.text
-                    if publishedx is not None:
+                    if publishedx:
                         entry['published'] = publishedx.text
                     event['entries'].append(entry)
 	        
-        return self.func(event)
+        return self.callback(event)
     
-    def on_notification(self,func):
-        self.func = func
+    def on_notification(self, callback):
+        self.callback = callback
 	
